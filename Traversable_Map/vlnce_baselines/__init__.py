9f2695274db6b0a1ea3e087bfcd3f248b18de279
Zihan Wang <wzhyh@outlook.com>
2024-06-16 15:05:30 +0800

    Update __init__.py

--- Traversable_Map/vlnce_baselines/__init__.py
+++ Traversable_Map/vlnce_baselines/__init__.py
@@ -1,4 +1,4 @@
-from vlnce_baselines import ss_trainer_ETP, dagger_trainer
+from vlnce_baselines import ss_trainer_CWP, dagger_trainer
 from vlnce_baselines.common import environments
 
 from vlnce_baselines.models import (
