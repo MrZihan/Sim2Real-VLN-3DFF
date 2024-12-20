e57a67d6e54a5860fa59f3875b966c8920682f40
Zihan Wang <wzhyh@outlook.com>
2024-12-20 16:05:21 +0800

    Update main.bash

--- VLN_3DFF/run_r2r/main.bash
+++ VLN_3DFF/run_r2r/main.bash
@@ -25,10 +25,10 @@ flag1="--exp_name release_r2r
 flag2=" --exp_name release_r2r
       --run-type eval
       --exp-config run_r2r/iter_train.yaml
-      SIMULATOR_GPU_IDS [0,1,2,3]
-      TORCH_GPU_IDS [0,1,2,3]
-      GPU_NUMBERS 4
-      NUM_ENVIRONMENTS 2
+      SIMULATOR_GPU_IDS [0]
+      TORCH_GPU_IDS [0]
+      GPU_NUMBERS 1
+      NUM_ENVIRONMENTS 1
       TASK_CONFIG.SIMULATOR.HABITAT_SIM_V0.ALLOW_SLIDING True
       EVAL.CKPT_PATH_DIR data/logs/r2r/checkpoints/release_r2r/ckpt.iter25000.pth
 	  MODEL.pretrained_path pretrained/model_step_100000.pt
@@ -38,10 +38,10 @@ flag2=" --exp_name release_r2r
 flag3="--exp_name release_r2r
       --run-type inference
       --exp-config run_r2r/iter_train.yaml
-      SIMULATOR_GPU_IDS [0,1,2,3]
-      TORCH_GPU_IDS [0,1,2,3]
-      GPU_NUMBERS 4
-      NUM_ENVIRONMENTS 2
+      SIMULATOR_GPU_IDS [0]
+      TORCH_GPU_IDS [0]
+      GPU_NUMBERS 1
+      NUM_ENVIRONMENTS 1
       TASK_CONFIG.SIMULATOR.HABITAT_SIM_V0.ALLOW_SLIDING True
       INFERENCE.CKPT_PATH data/logs/r2r/checkpoints/release_r2r/ckpt.iter25000.pth
       INFERENCE.PREDICTIONS_FILE preds.json
@@ -57,10 +57,10 @@ case $mode in
       ;;
       eval)
       echo "###### eval mode ######"
-      CUDA_VISIBLE_DEVICES='0,1,2,3' python -m torch.distributed.launch --nproc_per_node=4 --master_port $2 run.py $flag2
+      CUDA_VISIBLE_DEVICES='0' python -m torch.distributed.launch --nproc_per_node=1 --master_port $2 run.py $flag2
       ;;
       infer)
       echo "###### infer mode ######"
-      CUDA_VISIBLE_DEVICES='0,1,2,3' python -m torch.distributed.launch --nproc_per_node=4 --master_port $2 run.py $flag3
+      CUDA_VISIBLE_DEVICES='0' python -m torch.distributed.launch --nproc_per_node=1 --master_port $2 run.py $flag3
       ;;
 esac
