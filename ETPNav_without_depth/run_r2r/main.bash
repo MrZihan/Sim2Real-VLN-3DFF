698c3e3b6d516df432ee93a28ebd14bb7046a781
Zihan Wang <wzhyh@outlook.com>
2025-03-06 15:27:48 +0800

    Update main.bash

--- ETPNav_without_depth/run_r2r/main.bash
+++ ETPNav_without_depth/run_r2r/main.bash
@@ -1,12 +1,13 @@
 export GLOG_minloglevel=2
 export MAGNUM_LOG=quiet
+export CUDA_DEVICE_ORDER="PCI_BUS_ID"
 
 flag1="--exp_name release_r2r
       --run-type train
       --exp-config run_r2r/iter_train.yaml
-      SIMULATOR_GPU_IDS [0,1,2,3]
-      TORCH_GPU_IDS [0,1,2,3]
-      GPU_NUMBERS 4
+      SIMULATOR_GPU_IDS [0,1]
+      TORCH_GPU_IDS [0,1]
+      GPU_NUMBERS 2
       NUM_ENVIRONMENTS 8
       IL.iters 15000
       IL.lr 1e-5
@@ -14,12 +15,11 @@ flag1="--exp_name release_r2r
       IL.ml_weight 1.0
       IL.sample_ratio 0.75
       IL.decay_interval 3000
-      IL.load_from_ckpt True
+      IL.load_from_ckpt False
       IL.is_requeue True
       IL.waypoint_aug  True
-	  IL.ckpt_to_load data/logs/checkpoints/release_r2r/ckpt.iter10000.pth
       TASK_CONFIG.SIMULATOR.HABITAT_SIM_V0.ALLOW_SLIDING True
-      MODEL.pretrained_path pretrained/model_step_100000.pt
+      MODEL.pretrained_path pretrained/r2r_ce/mlm.sap_habitat_depth/ckpts/model_step_100000.pt
       "
 
 flag2=" --exp_name release_r2r
@@ -30,18 +30,17 @@ flag2=" --exp_name release_r2r
       GPU_NUMBERS 2
       NUM_ENVIRONMENTS 8
       TASK_CONFIG.SIMULATOR.HABITAT_SIM_V0.ALLOW_SLIDING True
-	  MODEL.pretrained_path pretrained/model_step_100000.pt
-      EVAL.CKPT_PATH_DIR data/logs/checkpoints/release_r2r/ckpt.iter15000.pth
+      EVAL.CKPT_PATH_DIR data/logs/checkpoints/release_r2r/ckpt.iter14500.pth
       IL.back_algo control
       "
 
 flag3="--exp_name release_r2r
       --run-type inference
       --exp-config run_r2r/iter_train.yaml
-      SIMULATOR_GPU_IDS [0,1]
-      TORCH_GPU_IDS [0,1]
-      GPU_NUMBERS 2
-      NUM_ENVIRONMENTS 8
+      SIMULATOR_GPU_IDS [0]
+      TORCH_GPU_IDS [0]
+      GPU_NUMBERS 1
+      NUM_ENVIRONMENTS 16
       TASK_CONFIG.SIMULATOR.HABITAT_SIM_V0.ALLOW_SLIDING True
       INFERENCE.CKPT_PATH data/logs/checkpoints/release_r2r/ckpt.iter12000.pth
       INFERENCE.PREDICTIONS_FILE preds.json
@@ -52,11 +51,11 @@ mode=$1
 case $mode in 
       train)
       echo "###### train mode ######"
-      CUDA_VISIBLE_DEVICES=4,5,6,7 python3 -m torch.distributed.launch --nproc_per_node=4 --master_port $2 run.py $flag1
+      CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port $2 run.py $flag1
       ;;
       eval)
       echo "###### eval mode ######"
-      CUDA_VISIBLE_DEVICES=2,3 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 2445 run.py $flag2
+      CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 2445 run.py $flag2
       ;;
       infer)
       echo "###### infer mode ######"
